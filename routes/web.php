7b148adf53b5316dcb88bc95b9ca94cf14eeeaa8
Kuro <kmh61030@gmail.com>
2024-04-25 19:23:42 +0630

    Define Roles, Make Models, & Format Nav-Link

--- routes/web.php
+++ routes/web.php
@@ -6,12 +6,34 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('/admin', function () {
+    return view('admin.index');
+})->name('admin.index');
+
+Route::get('/customer', function () {
+    return view('customer.index');
+})->name('customer.index');
+
+// Route::get('/', function () {
+//     if ( auth()->user()->is_admin) { 
+//         return redirect()->route('admin.index');
+//     }
+//     return view('welcome');
+// });
+// Route::resources([
+//     '/management/category' => CategoryController::class,
+// ]);
+
+
 Route::middleware([
     'auth:sanctum',
     config('jetstream.auth_session'),
     'verified',
 ])->group(function () {
     Route::get('/dashboard', function () {
-        return view('dashboard');
+        if ( auth()->user()->is_admin) { 
+            return redirect()->route('admin.index');
+        }
+        return view('customer.index');
     })->name('dashboard');
 });
