dd2e2331232e47d961323b444d2c65f2097cbc5e
Kuro <kmh61030@gmail.com>
2024-05-05 19:22:49 +0630

    some changes

--- app/Http/Controllers/Customer/SpecialsController.php
+++ app/Http/Controllers/Customer/SpecialsController.php
@@ -9,18 +9,6 @@ use App\Models\MenuModel;
 
 class SpecialsController extends Controller
 {
-    public function index()
-    {
-        $category = CategoryModel::where('name', 'Specials')->first();
-        if ($category) {
-            $specials = MenuModel::where('category_id', $category->id)->get();
-        } else {
-            $specials = collect(); // empty collection
-        }
-        return view('customer.index', ['specials' => $specials]);
-
-    }
-
     public function thankyou()
     {
         return view('thankyou');
